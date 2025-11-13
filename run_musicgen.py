ffbdccaced8d4cec0eff4f36e30e469bc571068a
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2025-11-13 23:02:51 +0100

    Successfully ran audiocraft

---
+++ run_musicgen.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("/home/jproboszcz/sem9/wimu/audiocraft")
+
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+
+m = MusicGen.get_pretrained("small")
+m.set_generation_params(duration=8)
+w = m.generate(["a powerful metal song"])
+audio_write("output", w[0].cpu(), m.sample_rate, strategy="loudness")
