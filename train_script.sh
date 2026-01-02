d54f8f611f740b30a8147ca4488032428a8a230e
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2026-01-02 22:30:58 +0100

    Further fixes to train script

--- train_script.sh
+++ train_script.sh
@@ -1,8 +1,10 @@
 TRAIN_SET_FRACTION=0.5
+TRAIN_SET_SIZE=`python3 -c "print(round($TRAIN_SET_FRACTION * 3659))"`
 
 for seed in {1..10}; do
     python3 sample_train_set.py egs/musiccaps/train/data_full.jsonl egs/musiccaps/train/data.jsonl "$TRAIN_SET_FRACTION" --seed "$seed"
 
+    AUDIOCRAFT_DORA_DIR=/data/jproboszcz/musicgen \
     dora run solver=musicgen/musicgen_base_32khz \
         model/lm/model_scale=small \
         continue_from=//pretrained/facebook/musicgen-small \
@@ -10,12 +12,12 @@ for seed in {1..10}; do
         dset=audio/musiccaps_custom \
         dataset.num_workers=2 \
         dataset.valid.num_samples=915 \
-        dataset.batch_size=8 \
+        dataset.batch_size=2 \
         schedule.cosine.warmup=8 \
         optim.optimizer=adamw \
         optim.lr=1e-4 \
-        optim.epochs=10 \
-        optim.updates_per_epoch=3659 \
+        optim.epochs=15 \
+        optim.updates_per_epoch=$TRAIN_SET_SIZE \
         optim.adam.weight_decay=0.01 \
         checkpoint.save_every=1 \
         checkpoint.keep_last=10 \
