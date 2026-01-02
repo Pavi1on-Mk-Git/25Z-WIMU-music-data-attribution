d64372c6640966008d64c53b5accade502e1a043
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2026-01-02 22:18:44 +0100

    Fix musicgen train script

--- train_script.sh
+++ train_script.sh
@@ -1,9 +1,9 @@
-TRAIN_SET_FRACTION=0.8
+TRAIN_SET_FRACTION=0.5
 
 for seed in {1..10}; do
-    python3 sample_train_set.py egs/musiccaps/train/data.jsonl egs/musiccaps/train/data.jsonl 0.8 --seed "$seed"
+    python3 sample_train_set.py egs/musiccaps/train/data_full.jsonl egs/musiccaps/train/data.jsonl "$TRAIN_SET_FRACTION" --seed "$seed"
 
-    CUDA_VISIBLE_DEVICES=1 dora run solver=musicgen/musicgen_base_32khz \
+    dora run solver=musicgen/musicgen_base_32khz \
         model/lm/model_scale=small \
         continue_from=//pretrained/facebook/musicgen-small \
         conditioner=text2music \
@@ -16,5 +16,8 @@ for seed in {1..10}; do
         optim.lr=1e-4 \
         optim.epochs=10 \
         optim.updates_per_epoch=3659 \
-        optim.adam.weight_decay=0.01
+        optim.adam.weight_decay=0.01 \
+        checkpoint.save_every=1 \
+        checkpoint.keep_last=10 \
+        checkpoint.keep_every_states="[model]"
 done
