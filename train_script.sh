bb2562d1dbd561e9d522cc1c6ddc547a28721b00
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2026-01-03 09:37:21 +0100

    Fix and lint train script

--- train_script.sh
+++ train_script.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 TRAIN_SET_FRACTION=0.5
-TRAIN_SET_SIZE=`python3 -c "print(round($TRAIN_SET_FRACTION * 3659))"`
+TRAIN_SET_SIZE=$(python3 -c "print(round($TRAIN_SET_FRACTION * 3659))")
 
 for seed in {1..10}; do
-    python3 sample_train_set.py egs/musiccaps/train/data_full.jsonl egs/musiccaps/train/data.jsonl "$TRAIN_SET_FRACTION" --seed "$seed"
+    mkdir -p egs/musiccaps/train_"$seed"
+    python3 sample_train_set.py egs/musiccaps/train/data.jsonl egs/musiccaps/train_"$seed"/data.jsonl "$TRAIN_SET_FRACTION" --seed "$seed"
 
     AUDIOCRAFT_DORA_DIR=/data/jproboszcz/musicgen \
     dora run solver=musicgen/musicgen_base_32khz \
@@ -17,9 +20,10 @@ for seed in {1..10}; do
         optim.optimizer=adamw \
         optim.lr=1e-4 \
         optim.epochs=15 \
-        optim.updates_per_epoch=$TRAIN_SET_SIZE \
+        optim.updates_per_epoch="$TRAIN_SET_SIZE" \
         optim.adam.weight_decay=0.01 \
         checkpoint.save_every=1 \
         checkpoint.keep_last=10 \
-        checkpoint.keep_every_states="[model]"
+        checkpoint.keep_every_states="[model]" \
+        datasource.train=egs/musiccaps/train_"$seed"
 done
