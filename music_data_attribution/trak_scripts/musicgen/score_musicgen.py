14cf69c0742e73ee7e5a9a86f49ebb7c9a44d8b3
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2026-01-07 10:55:57 +0100

    Add scoring script

--- music_data_attribution/trak_scripts/musicgen/score_musicgen.py
+++ music_data_attribution/trak_scripts/musicgen/score_musicgen.py
@@ -14,14 +14,15 @@ import sys
 import argparse
 
 SEED = 201
+EXPERIMENT_NAME = "musicgen_trak"
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("checkpoint", type=str, help="Path to the MusicGen checkpoint to featurize.")
+    parser.add_argument("checkpoint", type=str, help="Path to the MusicGen checkpoint to generate from.")
     parser.add_argument("--train-run-id", type=int, help="ID of the training run that produced the checkpoint.")
     parser.add_argument("--checkpoint-id", type=int, help="Number of epochs after which the checkpoint was saved.")
-    parser.add_argument("--music-data-path", type=str, help="Path to the MusicCaps wav files of the train split.")
+    parser.add_argument("--music-data-path", type=str, help="Path to the generated wav files.")
     parser.add_argument("--descriptions-path", type=str, help="Path to the MusicCaps captions csv file.")
     parser.add_argument("--trak-dir", type=str, help="Directory for TRAK intermediate results.")
     parser.add_argument("--batch-size", type=int, help="Batch size for gradient calculations.", default=2)
@@ -31,7 +32,7 @@ if __name__ == "__main__":
     torch.manual_seed(SEED)
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-    logger = logging.getLogger("featurize_musicgen")
+    logger = logging.getLogger("score_musicgen")
 
     dataset = MusicCapsDataset(
         audio_dir=args.music_data_path,
@@ -73,12 +74,18 @@ if __name__ == "__main__":
 
     checkpoint = load_checkpoint(args.checkpoint)
     model_id = (args.train_run_id - 1) * 10 + (args.checkpoint_id - 6)
-    traker.load_checkpoint(checkpoint["model"], model_id=model_id)
 
-    logger.debug("loaded checkpoint with traker")
+    traker.start_scoring_checkpoint(
+        exp_name=EXPERIMENT_NAME,
+        checkpoint=checkpoint["model"],
+        model_id=model_id,
+        num_targets=len(dataloader.dataset),
+    )
 
-    for indices, audios, descriptions in tqdm(dataloader, desc="Computing TRAK embeddings..."):
-        traker.featurize(batch=(audios.cuda(), descriptions), inds=indices)
-    traker.finalize_features(model_ids=[model_id])
+    logger.debug("started scoring checkpoint")
+
+    for indices, audios, descriptions in tqdm(dataloader, desc="Scoring..."):
+        traker.score(batch=(audios.cuda(), descriptions), inds=indices)
+    traker.finalize_scores(exp_name=EXPERIMENT_NAME, model_ids=[model_id])
 
     logger.info("finished")
