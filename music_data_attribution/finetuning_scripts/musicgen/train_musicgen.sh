f09fe410093981cd9e5fdcd45286f76a4ef149d7
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2026-01-07 11:20:17 +0100

    Fix updates_per_epoch in musicgen finetune

--- music_data_attribution/finetuning_scripts/musicgen/train_musicgen.sh
+++ music_data_attribution/finetuning_scripts/musicgen/train_musicgen.sh
@@ -28,8 +28,13 @@ if [ ! -f "$FULL_TRAIN_SET_FILE" ] || [ ! -f "$FULL_TEST_SET_FILE" ]; then
 fi
 
 FULL_TRAIN_SET_SIZE=$(wc -l < "$FULL_TRAIN_SET_FILE")
+FULL_TEST_SET_SIZE=$(wc -l < "$FULL_TEST_SET_FILE")
 TRAIN_SET_FRACTION=0.5
 TRAIN_SET_SIZE=$(python3 -c "print(round($TRAIN_SET_FRACTION * $FULL_TRAIN_SET_SIZE))")
+
+BATCH_SIZE=4
+TRAIN_SET_SIZE_BATCHES=$(python3 -c "import math; print(math.ceil($TRAIN_SET_SIZE / $BATCH_SIZE))")
+FULL_TRAIN_SET_SIZE_BATCHES=$(python3 -c "import math; print(math.ceil($FULL_TRAIN_SET_SIZE / $BATCH_SIZE))")
 
 cd "$AUDIOCRAFT_REPO_DIR"
 
@@ -61,13 +66,13 @@ for seed in {1..10}; do
         conditioner=text2music \
         dset=audio/musiccaps_custom \
         dataset.num_workers=2 \
-        dataset.valid.num_samples=915 \
-        dataset.batch_size=2 \
+        dataset.valid.num_samples="$FULL_TEST_SET_SIZE" \
+        dataset.batch_size="$BATCH_SIZE" \
         schedule.cosine.warmup=8 \
         optim.optimizer=adamw \
         optim.lr=1e-4 \
         optim.epochs=15 \
-        optim.updates_per_epoch="$TRAIN_SET_SIZE" \
+        optim.updates_per_epoch="$TRAIN_SET_SIZE_BATCHES" \
         optim.adam.weight_decay=0.01 \
         checkpoint.save_every=1 \
         checkpoint.keep_last=10 \
@@ -81,11 +86,11 @@ dora run solver=musicgen/musicgen_base_32khz \
     conditioner=text2music \
     dset=audio/musiccaps_custom \
     dataset.num_workers=2 \
-    dataset.valid.num_samples=915 \
-    dataset.batch_size=2 \
+    dataset.valid.num_samples="$FULL_TEST_SET_SIZE" \
+    dataset.batch_size="$BATCH_SIZE" \
     schedule.cosine.warmup=8 \
     optim.optimizer=adamw \
     optim.lr=1e-4 \
     optim.epochs=15 \
-    optim.updates_per_epoch="$FULL_TRAIN_SET_SIZE" \
+    optim.updates_per_epoch="$FULL_TRAIN_SET_SIZE_BATCHES" \
     optim.adam.weight_decay=0.01
