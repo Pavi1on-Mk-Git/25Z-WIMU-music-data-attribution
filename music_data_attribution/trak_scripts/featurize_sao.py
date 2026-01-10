b2d443d194323c02f4937f6b3376eb9ef9f3e4e5
Mikolaj Piorczynski <m.piorczynski13@gmail.com>
2026-01-10 21:44:55 +0100

    Add working scripts for computing TRAK embeddings for SAO-small

--- music_data_attribution/trak_scripts/featurize_sao.py
+++ music_data_attribution/trak_scripts/featurize_sao.py
@@ -1,17 +1,16 @@
 import argparse
 import json
 
+from loguru import logger
+from tqdm import tqdm
 import torch
-import torch.nn.functional as F
 from pytorch_lightning import seed_everything
+from music_data_attribution.modelout_functions.sao import SAOSmallModelOutput
+from trak import TRAKer
 
+from stable_audio_tools.models.utils import load_ckpt_state_dict
 from stable_audio_tools import get_pretrained_model
 from stable_audio_tools.data.dataset import create_dataloader_from_config
-from stable_audio_tools.inference.sampling import (
-    get_alphas_sigmas,
-    truncated_logistic_normal_rescaled,
-    sample_timesteps_logsnr,
-)
 
 
 def parse_args():
@@ -29,19 +28,13 @@ def parse_args():
         default=2,
     )
     parser.add_argument(
-        "--num-workers",
-        type=int,
-        help="Number of workers for the dataloader.",
-        default=4,
-    )
-    parser.add_argument(
         "--random-subset-percentage",
         type=float,
         help="Percentage of the dataset to use as a random subset.",
         default=1.0,
     )
     parser.add_argument(
-        "--checkpoint",
+        "--model-ckpt-path",
         type=str,
         help="Path to the MusicGen checkpoint to featurize.",
         default=None,
@@ -58,6 +51,24 @@ def parse_args():
         help="Number of epochs after which the checkpoint was saved.",
         default=None,
     )
+    parser.add_argument(
+        "--trak-dir",
+        type=str,
+        help="Directory for TRAK intermediate results.",
+        default="trak_results",
+    )
+    parser.add_argument(
+        "--proj-dim",
+        type=int,
+        help="Projection dimension for TRAK.",
+        default=4096,
+    )
+    parser.add_argument(
+        "--num-timesteps",
+        type=int,
+        help="Number of timesteps to average gradients over.",
+        default=1,
+    )
 
     return parser.parse_args()
 
@@ -68,132 +79,65 @@ def main(args):
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    # with open(args.model_config) as f:
-    #     model_config = json.load(f)
-    # model = create_model_from_config(model_config)
-    # if args.model_ckpt_path:
-    # copy_state_dict(model, load_ckpt_state_dict(args.model_ckpt_path))
-
+    # load model
     model, model_config = get_pretrained_model(
         "stabilityai/stable-audio-open-small", checkpoint_name="base_model"
     )
     model = model.to(device)
     model.eval()
 
-    # load checkpoint
-    # if args.checkpoint is not None:
-    #     model_ckpt_path = args.checkpoint
-    #     checkpoint = load_ckpt_state_dict(model_ckpt_path)
-    #     model.load_state_dict(checkpoint)
-    #     model_id = (args.train_run_id - 1) * 10 + (args.checkpoint_id - 6)
-    #     print(
-    #         f"Loaded model checkpoint from {model_ckpt_path} with model ID {model_id}"
-    #     )
-
-    print(f"Model config:\n{model_config}")
+    logger.info("Loaded pretrained model 'stabilityai/stable-audio-open-small' from checkpoint 'base_model'.")
 
     # prepare dataset and dataloader
     with open(args.dataset_config) as f:
         dataset_config = json.load(f)
 
-    print(f"Dataset config:\n{dataset_config}")
-
-    dataloader = create_dataloader_from_config(
+    dataloader, dataset_size = create_dataloader_from_config(
         dataset_config,
         batch_size=args.batch_size,
-        num_workers=args.num_workers,
         sample_rate=model_config["sample_rate"],
         sample_size=model_config["sample_size"],
         audio_channels=model_config.get("audio_channels", 2),
         shuffle=False,
         random_subset_percentage=args.random_subset_percentage,
+        return_dataset_size=True,
     )
+    logger.info("Dataloader created.")
 
-    batch = next(iter(dataloader))
-    print(batch)
+    # create TRAK
+    task = SAOSmallModelOutput(num_timesteps=args.num_timesteps)
 
-    def compute_loss_on_batch(model, batch, repeat_batch=1):
+    traker = TRAKer(
+        model=model,
+        task=task,
+        save_dir=args.trak_dir,
+        load_from_save_dir=True,
+        proj_max_batch_size=8,
+        train_set_size=dataset_size,
+        device=device,
+        proj_dim=args.proj_dim,
+    )
+    logger.info("Created TRAKer.")
+
+    # load checkpoint
+    if args.model_ckpt_path is not None:
+        checkpoint = load_ckpt_state_dict(args.model_ckpt_path)
+        model_id = args.train_run_id * 100 + args.checkpoint_id
+        traker.load_checkpoint(checkpoint, model_id=model_id)
+        logger.info(
+            f"Loaded checkpoint from {args.model_ckpt_path} under model ID {model_id} with TRAKer."
+        )
+    
+    for batch in tqdm(dataloader, desc="Computing TRAK embeddings..."):
         reals, metadata = batch
+        with torch.no_grad():
+            conditioning = model.conditioner(metadata, device)
 
-        if repeat_batch > 1:
-            reals = reals.repeat(repeat_batch, 1, 1)
-            metadata = metadata * repeat_batch
+        traker.featurize(batch=(reals.to(device), conditioning), num_samples=reals.shape[0])
+        torch.cuda.empty_cache()
 
-        reals = reals.to(device)
-        print(f"Audio shape: {reals.shape}")
-
-        if reals.ndim == 4 and reals.shape[0] == 1:
-            reals = reals[0]
-
-        diffusion_input = reals
-
-        conditioning = model.conditioner(metadata, device)
-
-        if model.pretransform is not None:
-            model.pretransform.to(device)
-
-            with torch.amp.autocast("cuda") and torch.set_grad_enabled(
-                model.pretransform.enable_grad
-            ):
-                model.pretransform.train(model.pretransform.enable_grad)
-
-                diffusion_input = model.pretransform.encode(diffusion_input)
-
-        if model_config["training"]["timestep_sampler"] == "uniform":
-            # Draw uniformly distributed continuous timesteps
-            t = torch.rand(reals.shape[0], device=device)
-        elif model_config["training"]["timestep_sampler"] == "logit_normal":
-            t = torch.sigmoid(torch.randn(reals.shape[0], device=device))
-        elif model_config["training"]["timestep_sampler"] == "trunc_logit_normal":
-            # Draw from logistic truncated normal distribution
-            t = truncated_logistic_normal_rescaled(reals.shape[0]).to(device)
-            # Flip the distribution
-            t = 1 - t
-        elif model_config["training"]["timestep_sampler"] == "log_snr":
-            t = sample_timesteps_logsnr(reals.shape[0]).to(device)
-        else:
-            raise ValueError(
-                f"Invalid timestep_sampler: {model_config['training']['timestep_sampler']}"
-            )
-
-        if model.dist_shift is not None:
-            # Shift the distribution
-            t = model.dist_shift.time_shift(t, reals.shape[2])
-
-        print(f"Timestep shape: {t.shape}")
-
-        # Calculate the noise schedule parameters for those timesteps
-        if model.diffusion_objective in ["v"]:
-            alphas, sigmas = get_alphas_sigmas(t)
-        elif model.diffusion_objective in ["rectified_flow", "rf_denoiser"]:
-            alphas, sigmas = 1 - t, t
-
-        # Combine the ground truth data and the noise
-        alphas = alphas[:, None, None]
-        sigmas = sigmas[:, None, None]
-        noise = torch.randn_like(diffusion_input)
-        noised_inputs = diffusion_input * alphas + noise * sigmas
-
-        if model.diffusion_objective == "v":
-            targets = noise * alphas - diffusion_input * sigmas
-        elif model.diffusion_objective in ["rectified_flow", "rf_denoiser"]:
-            targets = noise - diffusion_input
-
-        extra_args = {}
-
-        with torch.amp.autocast("cuda"):
-            output = model(
-                noised_inputs, t, cond=conditioning, cfg_dropout_prob=0, **extra_args
-            )
-            print("Computed model output.")
-            print(f"Output shape: {output.shape}")
-            print(f"Targets shape: {targets.shape}")
-
-        return F.mse_loss(output, targets)
-
-    loss = compute_loss_on_batch(model, batch, repeat_batch=32)
-    print(f"Computed loss on batch: {loss.item()}")
-
+    traker.finalize_features(model_ids=[model_id])
+    logger.info("Finished.")
 
 if __name__ == "__main__":
     main(parse_args())
--- music_data_attribution/trak_scripts/featurize_sao.sh
+++ music_data_attribution/trak_scripts/featurize_sao.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
+export FLASH_ATTN_DISABLE=1
+
 pdm run ./music_data_attribution/trak_scripts/featurize_sao.py \
     --dataset-config music_data_attribution/finetuning_scripts/sao/dataset_configs/datset_config.json \
-    --batch-size 1 \
-    --num-workers 4 \
-    --random-subset-percentage 0.1
+    --batch-size 2 \
+    --random-subset-percentage 0.1 \
+    --model-ckpt-path checkpoints/sao/0/epoch=9-step=4570.ckpt \
+    --train-run-id 0 \
+    --checkpoint-id 9 \
+    --proj-dim 4096 \
+    --num-noise-samples 6 \
+    --trak-dir ./results/trak_sao
