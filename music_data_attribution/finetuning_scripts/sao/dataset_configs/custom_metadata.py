e7ce2fbc0a0c8601cccd96ad30a9cc852bafd445
Mikolaj Piorczynski <m.piorczynski13@gmail.com>
2026-01-10 13:28:33 +0100

    Add dataset config

---
+++ music_data_attribution/finetuning_scripts/sao/dataset_configs/custom_metadata.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from typing import Any
+
+DESCRIPTIONS_PATH = "/home/jproboszcz/musiccaps/musiccaps-public.csv"
+DATASET = pd.read_csv(DESCRIPTIONS_PATH).to_dict(orient="records")
+
+
+def get_custom_metadata(info, audio):
+    song_id = int(info["relpath"].removesuffix(".wav"))
+    prompt = DATASET[song_id]["caption"]
+    return {"prompt": prompt}
+
+
+if __name__ == "__main__":
+    # Example usage
+    sample_info = {"relpath": "1.wav"}
+    sample_audio = None  # Placeholder for audio data
+    metadata = get_custom_metadata(sample_info, sample_audio)
+    print(metadata)
