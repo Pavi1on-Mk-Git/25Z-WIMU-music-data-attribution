423950b0d529d109048e01e4b54bb0b0be827efa
Mikolaj Piorczynski <m.piorczynski13@gmail.com>
2026-01-11 01:19:54 +0100

    Cleanup

--- music_data_attribution/trak_scripts/featurize_sao.sh
+++ music_data_attribution/trak_scripts/featurize_sao.sh
@@ -4,12 +4,12 @@ set -e
 export FLASH_ATTN_DISABLE=1
 
 pdm run ./music_data_attribution/trak_scripts/featurize_sao.py \
-    --dataset-config music_data_attribution/finetuning_scripts/sao/dataset_configs/datset_config.json \
+    --dataset-config music_data_attribution/finetuning_scripts/sao/dataset_config.json \
     --batch-size 2 \
     --random-subset-percentage 0.1 \
-    --model-ckpt-path checkpoints/sao/0/epoch=9-step=4570.ckpt \
+    --model-ckpt-path checkpoints/sao_small/0/epoch=9-step=4570.ckpt \
     --train-run-id 0 \
     --checkpoint-id 9 \
     --proj-dim 4096 \
-    --num-noise-samples 6 \
+    --num-timesteps 6 \
     --trak-dir ./results/trak_sao
