b0132bd6f3eed0bc82e14c15a529d28217cece7c
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2026-01-12 22:07:33 +0100

    Fix debug dataset preparation

--- music_data_attribution/finetuning_scripts/select_random_musiccaps_subset.sh
+++ music_data_attribution/finetuning_scripts/select_random_musiccaps_subset.sh
@@ -1,1 +1,5 @@
-ls -1 data/processed/music_data_train/ | shuf -n 64 | xargs -I {} cp "data/processed/music_data_train/{}" "data/processed/music_data_debug/"
+rm -rf data/processed/musiccaps/music_data_debug
+mkdir data/processed/musiccaps/music_data_debug
+ls -1 data/processed/musiccaps/music_data_train/ |
+    shuf -n 64 |
+    xargs -I {} cp "data/processed/musiccaps/music_data_train/{}" "data/processed/musiccaps/music_data_debug/"
