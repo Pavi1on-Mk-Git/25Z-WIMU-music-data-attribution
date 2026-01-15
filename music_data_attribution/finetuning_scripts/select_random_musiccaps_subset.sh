6bf6fe37a1034c036f62da078993dc32516cb671
Paweł Kochański <pawilioxion@gmail.com>
2026-01-15 19:21:54 +0100

    Fix select_random_musiccaps_subset script

--- music_data_attribution/finetuning_scripts/select_random_musiccaps_subset.sh
+++ music_data_attribution/finetuning_scripts/select_random_musiccaps_subset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf data/processed/musiccaps/music_data_debug
-mkdir data/processed/musiccaps/music_data_debug
+mkdir -p data/processed/musiccaps/music_data_debug
 find data/processed/musiccaps/music_data_train/* -prune |
     shuf -n 64 |
-    xargs -I {} cp "data/processed/musiccaps/music_data_train/{}" "data/processed/musiccaps/music_data_debug/"
+    xargs -I {} cp "{}" "data/processed/musiccaps/music_data_debug/"
