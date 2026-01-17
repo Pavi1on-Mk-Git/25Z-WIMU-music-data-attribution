6679ab50cb12b6e1efdfa60b6bf66e0abbe94d54
Paweł Kochański <pawilioxion@gmail.com>
2026-01-17 18:22:54 +0100

    Add training the full checkpoint to sao training script

--- music_data_attribution/finetuning_scripts/sao/train_sao.sh
+++ music_data_attribution/finetuning_scripts/sao/train_sao.sh
@@ -28,3 +28,24 @@ for seed_id in {0..9}; do
     done
 
 done
+
+pdm run python3 ./stable-audio-tools/train.py \
+    --config-file ./stable-audio-tools/defaults.ini \
+    --dataset-config "$SAO_FINETUNING_DIR"/dataset_config.json \
+    --model-config "$SAO_FINETUNING_DIR"/base_model_config.json \
+    --name stable_audio_open_small_finetune \
+    --save-dir "$CHECKPOINT_DIR" \
+    --batch-size 8 \
+    --precision 16-mixed \
+    --pretrained-ckpt-path results/checkpoints/base_model.ckpt \
+    --max-epochs 15 \
+    --checkpoint-every 15 \
+    --seed 10 \
+    --random-subset-percentage 1.0
+
+for checkpoint in "$CHECKPOINT_DIR"/*.ckpt; do
+    pdm run python3 ./stable-audio-tools/unwrap_model.py \
+        --model-config "$SAO_FINETUNING_DIR"/base_model_config.json \
+        --ckpt-path "$checkpoint" \
+        --name "${checkpoint%.*}"
+done
